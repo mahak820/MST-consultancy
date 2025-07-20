e4b8a23e849f4cd4812495e46ef5d9a150c44a38
mahak820 <mahaktuwani8@gmail.com>
2025-07-20 18:52:12 +0530

    tailwind

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Hero = () => {
   return (
     <div>
-      <h1>mahak tuwani</h1>
+      <h1 className='text-5xl text-sm font-bold'>mahak tuwani</h1>
     </div>
   )
 }
